3c1cbf79043f60b52df63ab59f538ce7962b7294
MurthyAvanithsa <murthyavanithsa@gmail.com>
2017-01-14 20:00:36 +0530

    Good to read

---
+++ good_to_read/README.md
@@ -0,0 +1,8 @@
+# Good blogs for python
+
+## H2 Python learn the hardway
+* https://learnpythonthehardway.org/book/
+
+## H2 Python module of the week
+*https://pymotw.com/2/
+
--- reading_csv/read_csv.py
+++ reading_csv/read_csv.py
@@ -1,9 +1,11 @@
 import csv
 
+# a simple CSV reader
 with open("user_data.csv") as csv_file:
-    csv_dic_reader = csv.DictReader(csv_file)
-    for row in csv_dic_reader:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
         print type(row), row
+
 
 # dic reader, helps you in reading every row as a dictionary
 with open("user_data.csv") as csv_file:
