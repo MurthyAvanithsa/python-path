bb6bb05f3fc89e4b8058d0489513a299e48b9534
MurthyAvanithsa <murthya@inviter.com>
2018-04-08 12:59:02 +0530

    Update settings.py

--- tasks/email_campaign/settings.py
+++ tasks/email_campaign/settings.py
@@ -1,3 +1,3 @@
-MAILGUN_API_KEY = "key-319082aa78ca3c0819e6069d0a4e14f5"
+MAILGUN_API_KEY = ""
 
-MAILGUN_URL = "https://api.mailgun.net/v3/sandbox923f311f457647788979536df9d12f73.mailgun.org/messages"
+MAILGUN_URL = ""
